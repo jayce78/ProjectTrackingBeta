468fd4b3b7cf4d309404d7903fb48c2370feef74
jayce78 <jasonleeworthy@hotmail.com>
2025-09-10 07:50:58 +0200

    Sidebar colour adaption

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { format, parseISO } from "date-fns";
 import { Trash2 } from "lucide-react";
 import { pctComplete } from "../utils/time";
